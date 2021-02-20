c1e98d576fbbc44722c48945e95964caab89a317
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-02-20 13:10:04 +0700

    TUGAS INI DIBUAT UNTUK MEMENUHI TUGAS UAS PBO

---
+++ src/uas/kalkulator/UASKALKULATOR.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.kalkulator;
+
+/**
+ * NAMA     : AGUS DERI DERMAWAN
+ * NIM      : 10119040
+ * KELAS    : IF1
+ * TANGGAL  : 20 - 02 - 2021
+ * NO SOAL  : 6
+ * @author User
+ */
+public class UASKALKULATOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
