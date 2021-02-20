c1dad38a85a459fa7b5f6d2eab4e0959cc3f4fe9
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-02-20 13:26:18 +0700

    TUGAS INI DIBUAT UNTUK MEMENUHI TUGAS UAS PBO

---
+++ src/uas/kalkulator/Kalkulator.java
@@ -0,0 +1,35 @@
+package uas.kalkulator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * NAMA     : AGUS DERI DERMAWAN
+ * NIM      : 10119040
+ * KELAS    : IF1
+ * TANGGAL  : 20 - 02 - 2021
+ * NO SOAL  : 6
+ * 
+ */
+public class Kalkulator {
+    public double value1, value2;
+    public double tambahBilangan(){
+        return value1 + value2;
+    }
+    public double kaliBilangan(){
+        return value1 * value2;
+    }
+    public double kurangBilangan(){
+        return value1 - value2;
+    }
+    public double bagiBilangan(){
+        return value1 / value2;
+    }
+    public double sisaBagiBilangan(){
+        return value1 % value2;
+    }
+    
+}
--- src/uas/kalkulator/UASKALKULATOR.java
+++ src/uas/kalkulator/UASKALKULATOR.java
@@ -5,13 +5,15 @@
  */
 package uas.kalkulator;
 
+import java.util.Scanner;
+
 /**
  * NAMA     : AGUS DERI DERMAWAN
  * NIM      : 10119040
  * KELAS    : IF1
  * TANGGAL  : 20 - 02 - 2021
  * NO SOAL  : 6
- * @author User
+ * 
  */
 public class UASKALKULATOR {
 
@@ -20,6 +22,23 @@ public class UASKALKULATOR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        Kalkulator kalkulator = new Kalkulator();
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kalkulator.value1 = scanner.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kalkulator.value2 = scanner.nextDouble();
+        double tambah = kalkulator.tambahBilangan();
+        double kurang = kalkulator.kurangBilangan();
+        double kali = kalkulator.kaliBilangan();
+        double bagi = kalkulator.bagiBilangan();
+        double sisaBagi = kalkulator.sisaBagiBilangan();
+        System.out.println("Hasil Pertambahan : " + tambah);
+        System.out.println("Hasil Pengurangan : " + kurang);
+        System.out.println("Hasil Perkalian : " + kali);
+        System.out.println("Hasil Pembagian : " + bagi);
+        System.out.println("Hasil Sisa : " + sisaBagi);
     }
     
 }
